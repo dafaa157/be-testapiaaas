697a3963f2fcdd0dcea510b9882fe0f2dccc4538
dafaa157 <l200230157@student.ums.ac.id>
2025-12-18 13:19:27 +0700

    Add StudentProfile model with user relationship

--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class StudentProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    nim = models.CharField(max_length=20, unique=True)
+    full_name = models.CharField(max_length=150)
+    prodi = models.CharField(max_length=120, blank=True)
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to="profiles/", blank=True)
+
+    def __str__(self):
+        return self.full_name
