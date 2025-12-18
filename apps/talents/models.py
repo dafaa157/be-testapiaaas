b1a59a35c92d4ba4901393f0e4d2a5c2c67331cb
dafaa157 <l200230157@student.ums.ac.id>
2025-12-18 13:20:09 +0700

    Create Skill, Experience, and Portfolio models
    Added Skill, Experience, and Portfolio models with relationships to StudentProfile.

--- apps/talents/models.py
+++ apps/talents/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from apps.profiles.models import StudentProfile
 
-# Create your models here.
+class Skill(models.Model):
+    name = models.CharField(max_length=100)
+    student = models.ManyToManyField(StudentProfile, related_name="skills")
+
+    def __str__(self):
+        return self.name
+
+
+class Experience(models.Model):
+    student = models.ForeignKey(StudentProfile, on_delete=models.CASCADE, related_name="experiences")
+    title = models.CharField(max_length=120)
+    institution = models.CharField(max_length=120)
+    start_date = models.DateField()
+    end_date = models.DateField(blank=True, null=True)
+    is_current = models.BooleanField(default=False)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Portfolio(models.Model):
+    student = models.ForeignKey(StudentProfile, on_delete=models.CASCADE, related_name="portfolios")
+    title = models.CharField(max_length=120)
+    link = models.URLField(blank=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
