d02153450ffb5533674a9fbdbd8f44a14bf53a54
dafaa157 <l200230157@student.ums.ac.id>
2025-12-18 13:08:13 +0700

    add allowed host

--- core/settings.py
+++ core/settings.py
@@ -30,7 +30,10 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".railway.app",
+    "be-testapiaaas-production.up.railway.app"
+]
 
 
 # Application definition
