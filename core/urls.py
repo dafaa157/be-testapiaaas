e7a3b33ea70c12fb394f6e9e8fb2460eff2b9d6f
dafaa157 <l200230157@student.ums.ac.id>
2025-12-18 13:07:32 +0700

    hapus root

--- core/urls.py
+++ core/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', index),
+    
     path('admin/', admin.site.urls),
     path('api/auth/', include('apps.accounts.urls')),
 ]
