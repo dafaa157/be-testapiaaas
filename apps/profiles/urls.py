4e7f382e3085d79b2f4e9a85fec4cec27dc3040c
dafaa157 <l200230157@student.ums.ac.id>
2025-12-20 00:07:57 +0700

    Remove profile_me URL path from urlpatterns

--- apps/profiles/urls.py
+++ apps/profiles/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import profile_me, list_students, profile_detail
+from .views import  list_students, profile_detail
 
 urlpatterns = [
-    path("me/", profile_me),
     path("list/", list_students),
     path("<str:nim>/", profile_detail),
 ]
