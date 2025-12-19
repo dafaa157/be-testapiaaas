f80f70f6d9363754d5bf84049174843bc3f91d09
dafaa157 <l200230157@student.ums.ac.id>
2025-12-20 00:05:48 +0700

    Refactor profile_me and update authentication handling
    Refactor profile_me function to remove authentication and allow public access. Update imports for better organization.

--- apps/profiles/views.py
+++ apps/profiles/views.py
@@ -1,5 +1,9 @@
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import (
+    api_view,
+    permission_classes,
+    authentication_classes,
+)
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -7,36 +11,18 @@ from .models import StudentProfile
 from .serializers import StudentProfileSerializer
 
 
-@api_view(["GET", "PUT"])
-@permission_classes([IsAuthenticated])
-def profile_me(request):
-    profile = StudentProfile.objects.get(user=request.user)
-
-    if request.method == "GET":
-        serializer = StudentProfileSerializer(profile)
-        return Response(serializer.data)
-
-    if request.method == "PUT":
-        serializer = StudentProfileSerializer(profile, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 @api_view(["GET"])
-@permission_classes([AllowAny]) # Bisa diganti AllowAny kalau mau public
+@authentication_classes([])      # matikan JWT auth
+@permission_classes([AllowAny])  # public endpoint
 def list_students(request):
-    # Ambil semua data student
     profiles = StudentProfile.objects.all()
-    
-    # Serialize datanya (many=True itu wajib karena datanya list/banyak)
     serializer = StudentProfileSerializer(profiles, many=True)
-    
     return Response(serializer.data)
 
+
 @api_view(["GET"])
-@permission_classes([AllowAny])
+@authentication_classes([])      # matikan JWT auth
+@permission_classes([AllowAny])  # public endpoint
 def profile_detail(request, nim):
     try:
         profile = StudentProfile.objects.get(nim=nim)
