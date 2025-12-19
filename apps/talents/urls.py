0993cf5ad762b2c6be6f368d00dc1e2b82e9898e
dafaa157 <l200230157@student.ums.ac.id>
2025-12-19 23:23:59 +0700

    Add public routes for skills, experiences, and portfolios

--- apps/talents/urls.py
+++ apps/talents/urls.py
@@ -1,9 +1,18 @@
-from django.urls import path
-from .views import skill_list_create, experience_list_create, portfolio_list_create
-
+from .views import (
+    skill_list_create,
+    experience_list_create,
+    portfolio_list_create,
+    public_skills,
+    public_experiences,
+    public_portfolios
+)
 
 urlpatterns = [
     path("skills/", skill_list_create),
     path("experiences/", experience_list_create),
     path("portfolios/", portfolio_list_create),
+
+    path("<str:nim>/skills/", public_skills),
+    path("<str:nim>/experiences/", public_experiences),
+    path("<str:nim>/portfolios/", public_portfolios),
 ]
