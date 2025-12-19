24066616e53dafbe2c573e064a2752d81e8f7343
dafaa157 <l200230157@student.ums.ac.id>
2025-12-20 06:36:00 +0700

    Add phone field to profile model

--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -9,6 +9,6 @@ class StudentProfile(models.Model):
     prodi = models.CharField(max_length=120, blank=True)
     bio = models.TextField(blank=True)
     profile_picture = models.ImageField(upload_to="profiles/", blank=True, null=True)
-
+    phone = models.CharField(max_length=20)
     def __str__(self):
         return self.full_name
