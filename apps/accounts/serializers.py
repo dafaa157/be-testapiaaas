126759e3ac846bf17cbe831a29330578db4c99fc
dafaa157 <l200230157@student.ums.ac.id>
2025-12-20 00:33:46 +0700

    Add NIM field to RegisterSerializer and update user creation

--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -3,22 +3,23 @@ from django.contrib.auth.models import User
 
 
 class RegisterSerializer(serializers.ModelSerializer):
+    full_name = serializers.CharField(write_only=True)
+    nim = serializers.CharField(write_only=True)
     password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ["id", "full_name", "email", "password"]
+        fields = ["id", "full_name", "nim", "email", "password"]
 
     def create(self, validated_data):
         user = User.objects.create_user(
-            fullname=validated_data["full_name"],
+            username=validated_data["nim"],     # gunakan nim sbg username
             email=validated_data["email"],
             password=validated_data["password"],
         )
+
+        # optional: simpan nama (boleh hapus kalau tidak perlu)
+        user.first_name = validated_data["full_name"]
+        user.save()
+
         return user
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ["id", "username", "email", "is_staff"]
