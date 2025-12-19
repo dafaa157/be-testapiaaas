698b85ee30932e29e99d8535af5a77bc4977d663
dafaa157 <l200230157@student.ums.ac.id>
2025-12-19 23:14:25 +0700

    Add nested serializers for skills, experiences, portfolios

--- apps/profiles/serializers.py
+++ apps/profiles/serializers.py
@@ -1,7 +1,22 @@
 from rest_framework import serializers
 from .models import StudentProfile
+from apps.talents.serializers import SkillSerializer, ExperienceSerializer, PortfolioSerializer
 
 class StudentProfileSerializer(serializers.ModelSerializer):
+    skills = SkillSerializer(many=True, read_only=True)
+    experiences = ExperienceSerializer(many=True, read_only=True)
+    portfolios = PortfolioSerializer(many=True, read_only=True)
+    
     class Meta:
         model = StudentProfile
-        fields = ["id", "full_name", "nim", "prodi", "bio", "profile_picture"]
+        fields = fields = [
+            "id",
+            "full_name",
+            "nim",
+            "prodi",
+            "bio",
+            "profile_picture",
+            "skills",
+            "experiences",
+            "portfolios",
+        ]
