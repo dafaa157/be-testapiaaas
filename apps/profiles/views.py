531fc1a866f1b5ae7b59ea1cc8dd832eb60c9aa2
dafaa157 <l200230157@student.ums.ac.id>
2025-12-20 01:23:01 +0700

    Add IsAuthenticated permission to views

--- apps/profiles/views.py
+++ apps/profiles/views.py
@@ -1,9 +1,10 @@
+
 from rest_framework.decorators import (
     api_view,
     permission_classes,
     authentication_classes,
 )
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 
