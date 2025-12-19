dbeadee18144facdb0fbf896bfea2919467c1ff9
dafaa157 <l200230157@student.ums.ac.id>
2025-12-20 06:21:49 +0700

    Simplify StudentProfileSerializer fields
    Removed nested serializers for skills, experiences, and portfolios from StudentProfileSerializer.

--- apps/profiles/serializers.py
+++ apps/profiles/serializers.py
@@ -1,23 +1,8 @@
 from rest_framework import serializers
 from .models import StudentProfile
-from apps.talents.serializers import SkillSerializer, ExperienceSerializer, PortfolioSerializer
+
 
 class StudentProfileSerializer(serializers.ModelSerializer):
-    skills = SkillSerializer(many=True, read_only=True)
-    experiences = ExperienceSerializer(many=True, read_only=True)
-    portfolios = PortfolioSerializer(many=True, read_only=True)
-    
     class Meta:
         model = StudentProfile
-        fields = fields = [
-            "id",
-            "full_name",
-            "nim",
-            "prodi",
-            "bio",
-            "profile_picture",
-            "phone", 
-            "skills", 
-            "experiences",
-            "portfolios"
-        ]
+        fields = ["id", "full_name", "nim", "prodi", "bio", "profile_picture"]
