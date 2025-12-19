597b533fcf51c77466179528e999b334708fdd0e
dafaa157 <l200230157@student.ums.ac.id>
2025-12-20 00:24:00 +0700

    Add authentication_classes to view decorators

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,4 +1,4 @@
-from rest_framework.decorators import api_view, permission_classes, permission_classes
+from rest_framework.decorators import api_view, permission_classes, permission_classes, authentication_classes
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated, AllowAny
