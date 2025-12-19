06c2385a0581a13597ab104ec9cbf6c5d0516d69
dafaa157 <l200230157@student.ums.ac.id>
2025-12-20 05:34:04 +0700

    Modify profile_picture field to be nullable
    Updated profile_picture field to allow null values.

--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -1,8 +1,6 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
 from django.contrib.auth.models import User
+
 
 class StudentProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -10,8 +8,7 @@ class StudentProfile(models.Model):
     full_name = models.CharField(max_length=150)
     prodi = models.CharField(max_length=120, blank=True)
     bio = models.TextField(blank=True)
-    phone = models.CharField(max_length=20, blank=True)
-    profile_picture = models.ImageField(upload_to="profiles/", blank=True)
+    profile_picture = models.ImageField(upload_to="profiles/", blank=True, null=True)
 
     def __str__(self):
         return self.full_name
