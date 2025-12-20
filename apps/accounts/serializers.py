122b86aeeaca278ef5671a5384ebf91872ae8521
dafaa157 <l200230157@student.ums.ac.id>
2025-12-20 11:09:50 +0700

    Import TokenObtainPairSerializer for custom token

--- apps/accounts/serializers.py
+++ apps/accounts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
