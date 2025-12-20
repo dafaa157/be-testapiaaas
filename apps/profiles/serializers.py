16dab43c23b37721399d832715fd033ffb51f8bc
dafaa157 <l200230157@student.ums.ac.id>
2025-12-20 09:20:15 +0700

    Add skills, experiences, and portfolios to serializer

--- apps/profiles/serializers.py
+++ apps/profiles/serializers.py
@@ -1,14 +1,18 @@
 from rest_framework import serializers
 from .models import StudentProfile
 
+from apps.talents.serializers import SkillSerializer, ExperienceSerializer, PortfolioSerializer
 
 class StudentProfileSerializer(serializers.ModelSerializer):
     # Make profile_picture URL readable
     profile_picture = serializers.ImageField(required=False)
+    skills = SkillSerializer(many=True, read_only=True)
+    experiences = ExperienceSerializer(many=True, read_only=True)
+    portfolios = PortfolioSerializer(many=True, read_only=True)
     
     class Meta:
         model = StudentProfile
-        fields = ["id", "full_name", "nim", "prodi", "bio", "profile_picture", "phone"]
+        fields = ["id", "full_name", "nim", "prodi", "bio", "profile_picture", "phone","skills", "experiences", "portfolios"]
         # Make these optional when updating
         extra_kwargs = {
             'phone': {'required': False},
