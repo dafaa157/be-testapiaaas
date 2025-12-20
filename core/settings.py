979bc5aa96a801ddadb9777bb79cf6ed2bbfa675
dafaa157 <l200230157@student.ums.ac.id>
2025-12-20 21:13:31 +0700

    Add raw media storage configuration to Cloudinary
    Add configuration for raw media storage in Cloudinary.

--- core/settings.py
+++ core/settings.py
@@ -59,6 +59,9 @@ CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
     'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+    'TheRawMediaCloudinaryStorage': {
+        'RESOURCE_TYPE': 'raw'
+    }
 }
 
 REST_FRAMEWORK = {
