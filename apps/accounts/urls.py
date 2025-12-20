7dd47ef96dff3a197d55632b8f1e53cd797ed4c4
dafaa157 <l200230157@student.ums.ac.id>
2025-12-20 11:49:19 +0700

    Update urls.py

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import login_view, register_view, me_view
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path("login/", login_view),
