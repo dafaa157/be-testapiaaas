4f6482efa75dbb18f5de797337d200aa87b305e7
dafaa157 <l200230157@student.ums.ac.id>
2025-12-20 07:42:50 +0700

    Uncomment MEDIA_URL setting in settings.py

--- core/settings.py
+++ core/settings.py
@@ -18,7 +18,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
-# MEDIA_URL = '/media/'
+MEDIA_URL = '/media/'
 # MEDIA_ROOT = '/app/media'  
 
 # Quick-start development settings - unsuitable for production
