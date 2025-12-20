87f0af88f7c5b737470f3cc15b0f8e18ba957b50
dafaa157 <l200230157@student.ums.ac.id>
2025-12-20 08:00:26 +0700

    Add parser_classes decorator to views.py

--- apps/profiles/views.py
+++ apps/profiles/views.py
@@ -1,4 +1,4 @@
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view, permission_classes, parser_classes
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework import status
