9e2101230102258d604304a8324f7621239bfe1d
dafaa157 <l200230157@student.ums.ac.id>
2025-12-20 19:24:34 +0700

    Add social and contact fields to profile model
    Added fields for email, LinkedIn, GitHub, and Instagram to the profile model.

--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -11,5 +11,10 @@ class StudentProfile(models.Model):
     profile_picture = models.ImageField(upload_to="profiles/", blank=True, null=True)
     custom_cv_file = models.FileField(upload_to="cv_uploads/", blank=True, null=True)
     phone = models.CharField(max_length=20, default='')
+
+    email = models.EmailField(max_length=100, blank=True, null=True)     # Email Publik
+    linkedin = models.URLField(max_length=200, blank=True, null=True)    # Link LinkedIn
+    github = models.URLField(max_length=200, blank=True, null=True)      # Link GitHub
+    instagram = models.URLField(max_length=200, blank=True, null=True)   # Link Instagram
     def __str__(self):
         return self.full_name
